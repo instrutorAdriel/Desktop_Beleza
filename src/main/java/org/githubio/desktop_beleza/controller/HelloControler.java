9bddfa38bc336b65e5e305daa00bf74ee7c97033
João felipe <joaofelip39@gmail.com>
2026-04-08 17:07:40 -0300

    gerenciamento de turmas

---
+++ src/main/java/org/githubio/desktop_beleza/controller/HelloControler.java
@@ -0,0 +1,14 @@
+package org.githubio.desktop_beleza.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloControler{
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
