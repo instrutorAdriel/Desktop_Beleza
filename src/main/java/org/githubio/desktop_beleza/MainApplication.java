99dda00ab24d26ace9d77bc4c2ebcbec5687c11e
Davi Lima <davidlima35566@gmail.com>
2026-04-22 14:56:22 -0300

    Correção da resolução para HD

--- src/main/java/org/githubio/desktop_beleza/MainApplication.java
+++ src/main/java/org/githubio/desktop_beleza/MainApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
     private static Scene scene;
-    private static final int MinWidth = 800;
-    private static final int MinHeight = 800;
+    private static final int MinWidth = 1366;
+    private static final int MinHeight = 768;
 
     @Override
     public void start(Stage stage) throws IOException {
