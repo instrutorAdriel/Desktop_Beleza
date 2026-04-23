26a602482ad6d4330bce659eb297640e8248f4bb
Davi Lima <davidlima35566@gmail.com>
2026-04-23 15:09:42 -0300

    Correção da chave primaria da tabela modelos para singular

--- docs/desktop-beleza.sql
+++ docs/desktop-beleza.sql
@@ -7,25 +7,37 @@ CREATE TABLE tb_instrutores (
     senha VARCHAR(128) NOT NULL
 );
 
-CREATE TABLE tb_turmas (
-	id_turma INTEGER PRIMARY KEY AUTO_INCREMENT,
-    turno ENUM("Matutino", "Vespertino", "Noturno"),
-    id_status_turma INTEGER,
-    FOREIGN KEY (id_status_turma) REFERENCES tb_status_turma(id_status_turma)
-);
-
 CREATE TABLE tb_modelos (
-	id_modelos INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_modelo INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome_modelo VARCHAR(96) NOT NULL,
     email VARCHAR(64) UNIQUE,
     telefone INT(10)
 );
 
 CREATE TABLE tb_servicos (
-	id_servico INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_servico INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome_servico VARCHAR(64) NOT NULL,
     descricao VARCHAR(255),
     horario_disponivel TIME
+);
+
+# Status da turma: Em Andamento, Finalizado
+CREATE TABLE tb_status_turma (
+    id_status_turma INTEGER PRIMARY KEY AUTO_INCREMENT,
+    status_turma VARCHAR(24) NOT NULL
+);
+
+# Status da Agenda: Pendente, Compareceu, Não Compareceu
+CREATE TABLE tb_status_agenda (
+    id_status_agenda INTEGER PRIMARY KEY AUTO_INCREMENT,
+    status_agenda VARCHAR(24) NOT NULL
+);
+
+CREATE TABLE tb_turmas (
+	id_turma INTEGER PRIMARY KEY AUTO_INCREMENT,
+    turno ENUM("Matutino", "Vespertino", "Noturno"),
+    id_status_turma INTEGER,
+    FOREIGN KEY (id_status_turma) REFERENCES tb_status_turma(id_status_turma)
 );
 
 CREATE TABLE tb_agenda (
@@ -38,7 +50,7 @@ CREATE TABLE tb_agenda (
     id_status_agenda INTEGER,
     FOREIGN KEY (id_servico) REFERENCES tb_servicos(id_servico),
     FOREIGN KEY (id_turmas_instrutores) REFERENCES rl_turmas_instrutores(id_turmas_instrutores),
-    FOREIGN KEY (id_modelo) REFERENCES tb_modelos(id_modelos),
+    FOREIGN KEY (id_modelo) REFERENCES tb_modelos(id_modelo),
     FOREIGN KEY (id_status_agenda) REFERENCES tb_status_agenda(id_status_agenda)
 );
 
@@ -48,18 +60,6 @@ CREATE TABLE rl_turmas_instrutores (
     id_turma INTEGER,
     FOREIGN KEY (id_instrutor) REFERENCES tb_instrutores(id_instrutor),
     FOREIGN KEY (id_turma) REFERENCES tb_turmas(id_turma)
-);
-
-# Status da turma: Em Andamento, Finalizado
-CREATE TABLE tb_status_turma ( 
-	id_status_turma INTEGER PRIMARY KEY AUTO_INCREMENT,
-    status_turma VARCHAR(24) NOT NULL
-);
-
-# Status da Agenda: Pendente, Compareceu, Não Compareceu
-CREATE TABLE tb_status_agenda (
-	id_status_agenda INTEGER PRIMARY KEY AUTO_INCREMENT,
-    status_agenda VARCHAR(24) NOT NULL
 );
 
 # Inserção dos dados
