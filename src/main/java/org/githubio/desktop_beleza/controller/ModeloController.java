c679b4c931410aadda60caf6e29e714f95fdfe43
kelvin <kelvinmacos0@gmail.com>
2026-04-28 14:31:01 -0300

    back end para abrir o PopUp

--- src/main/java/org/githubio/desktop_beleza/controller/ModeloController.java
+++ src/main/java/org/githubio/desktop_beleza/controller/ModeloController.java
@@ -1,8 +1,15 @@
 package org.githubio.desktop_beleza.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.githubio.desktop_beleza.MainApplication;
 import org.githubio.desktop_beleza.model.ModeloDAO;
+
+import java.io.IOException;
 
 public class ModeloController {
     @FXML
@@ -18,5 +25,30 @@ public class ModeloController {
         String telefone = campo_telefone.getText();
         String nome = campo_nome.getText();
         modeloDAO.cadastrarModelo(nome, telefone, email);
+    }
+
+    @FXML
+    void abrirTelaAdcionarPopUp() {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("telamodelo.fxml"));
+
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setWidth(800);
+            stage.setHeight(600);
+            stage.setTitle("tela adicionar modelo");
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+
+            // Quando o Pop-up fechar, recarregamos a lista original do banco
+            stage.setOnHiding(event -> onAdicionarmodelo());
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
