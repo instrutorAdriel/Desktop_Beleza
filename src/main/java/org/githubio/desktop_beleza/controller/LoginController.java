af9f57c81ea4248ea2309b33707fdb456891d345
Davi Lima <davidlima35566@gmail.com>
2026-04-29 16:05:29 -0300

    Correção das nomenclaturas dos arquivos e adaptação do código para nova tela de turmas

--- src/main/java/org/githubio/desktop_beleza/controller/LoginController.java
+++ src/main/java/org/githubio/desktop_beleza/controller/LoginController.java
@@ -44,7 +44,7 @@ public class LoginController {
         if (logado==true) {
             //IO.println("Login realizado com sucesso!");
             MainApplication.setUsuario(emailDigitado);
-            MainApplication.setRoot("gerenciarTurma");
+            MainApplication.setRoot("gerenciarturmas");
         }
         else {
             Alert alerta = new Alert(Alert.AlertType.WARNING);
